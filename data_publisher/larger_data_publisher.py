731cdfcae773af0343b000e7458d4e5d60d3a16b
Sarthak Gangopadhyay <sarthakgy@google.com>
2018-08-03 00:20:01 +0000

    i:q!

---
+++ data_publisher/larger_data_publisher.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+
+#import webapp2
+import requests
+import json
+import base64
+from google.cloud import pubsub_v1
+from collections import OrderedDict
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import time
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+PORT_NUMBER = 8080
+url= 'http://data.cityofchicago.org/resource/8v9j-bter.json'
+apigee_format_json_url= 'http://amer-demo25-test.apigee.net/format-json'
+gcp_project= 'camel-154800'
+pubsub_name='big_ant'
+
+class myHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = publisher.topic_path(gcp_project, pubsub_name)
+        
+        #Make the call to city Of Chicago to get the data
+        response = requests.get(url)
+        #Check if the response is 200
+        if (response.ok):
+                            full_message = response.json()
+
+                            # Loop through all the json objects in the array
+                            i = 0
+                            print('Total number of region data received is '+str(len(full_message)))
+                            while i< len(full_message): 
+                                print('Now working on message number #### '+str(i))
+      		                message=(full_message[i])   
+                                 
+                                #r=self.format_payload(full_message[i])
+                                print('calling format_payload function now')
+      		                
+                                ORDERED_KEYS = ['_direction', '_fromst', '_last_updt','_length','_lif_lat','_lit_lat','_lit_lon','_strheading','_tost','_traffic','segmentid','start_lon','street']
+                                ordered_json = OrderedDict((k, message[k]) for k in ORDERED_KEYS)
+                                text=json.dumps(ordered_json)
+                                #text = r.content.decode(requests.utils.guess_json_utf(r.content)).encode('utf-8')
+                                print('The response is ready to be published. This is how it looks now:')
+                                print(text)
+      		                
+      		                
+                                   #Get a client and publish the message to the topic
+
+                                   # publisher = pubsub_v1.PublisherClient()
+                                   # topic_path = publisher.topic_path(gcp_project, pubsub_name)
+                                publisher.publish(topic_path, data=text) 
+      		                i += 1    
+                                self.send_response(200)
+                                self.send_header('Content-type','text/html')
+                                self.end_headers()
+                                # Send the html message
+                                self.wfile.write("we just posted objects to PubSub successfully. Total objects posted"+str(i))
+   		                print("We posted the messages successfully to PubSub")
+	else :
+
+   	     response.raise_for_status()  
+
+try:
+#Create a web server and define the handler to manage the
+	#incoming request
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+	
+	#Wait forever for incoming htto requests
+	server.serve_forever()
+
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	server.socket.close()
+
---
+++ data_publisher/smaller_data_publisher.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python
+
+#import webapp2
+import requests
+import json
+import base64
+from google.cloud import pubsub_v1
+from collections import OrderedDict
+from BaseHTTPServer import BaseHTTPRequestHandler,HTTPServer
+import time
+# vim: tabstop=8 expandtab shiftwidth=4 softtabstop=4
+
+PORT_NUMBER = 8081
+url= 'http://amer-demo25-test.apigee.net/chicago'
+apigee_format_json_url= 'http://amer-demo25-test.apigee.net/format-json'
+gcp_project= 'camel-154800'
+pubsub_name='ant3'
+
+class myHandler(BaseHTTPRequestHandler):
+
+    def format_payload(self,message):
+       try:
+           r = requests.post(apigee_format_json_url, json=message)  
+           print('Got response back from Apigee format-json method')
+           return r
+       except requests.ConnectionError as e:
+              print("OOPS!! Connection Error. Make sure you are connected to Internet. Technical Details given below.\n")
+              print(str(e))            
+       except requests.Timeout as e:
+              print("OOPS!! Timeout Error")
+              print(str(e))
+       except requests.RequestException as e:
+              print("OOPS!! General Error")
+              print(str(e))
+
+    def do_GET(self):
+        publisher = pubsub_v1.PublisherClient()
+        topic_path = publisher.topic_path(gcp_project, pubsub_name)
+        
+        #Make the call to city Of Chicago to get the data
+        response = requests.get(url)
+        #Check if the response is 200
+        if (response.ok):
+                            full_message = response.json()
+
+                            # Loop through all the json objects in the array
+                            i = 0
+                            print('Total number of region data received is '+str(len(full_message)))
+                            while i< len(full_message): 
+                                print('Now working on message number #### '+str(i))
+      		                message=str(full_message[i])   
+                                 
+                                r=self.format_payload(full_message[i])
+                                print('calling format_payload function now')
+      		                
+                                text = r.content.decode(requests.utils.guess_json_utf(r.content)).encode('utf-8')
+                                print('The response is ready to be published. This is how it looks now:')
+                                print(text)
+      		                
+      		                
+                                   #Get a client and publish the message to the topic
+
+                                   # publisher = pubsub_v1.PublisherClient()
+                                   # topic_path = publisher.topic_path(gcp_project, pubsub_name)
+                                publisher.publish(topic_path, data=text) 
+      		                i += 1    
+                                self.send_response(200)
+                                self.send_header('Content-type','text/html')
+                                self.end_headers()
+                                # Send the html message
+                                self.wfile.write("we just posted objects to PubSub successfully. Total objects posted"+str(i))
+   		                print("We posted the messages successfully to PubSub")
+	else :
+
+   	     response.raise_for_status()  
+
+try:
+#Create a web server and define the handler to manage the
+	#incoming request
+	server = HTTPServer(('', PORT_NUMBER), myHandler)
+	print 'Started httpserver on port ' , PORT_NUMBER
+	
+	#Wait forever for incoming htto requests
+	server.serve_forever()
+
+except KeyboardInterrupt:
+	print '^C received, shutting down the web server'
+	server.socket.close()
+
